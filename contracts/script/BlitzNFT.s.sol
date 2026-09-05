eab0f72c133165e7142ac1bc97fbaf021cb5da9a
wangzhenxi <im.wangzhenxi@gmail.com>
2026-09-05 11:11:15 +0800

    deploy BlitzNFT to Monad testnet and verify on explorers

--- contracts/script/BlitzNFT.s.sol
+++ contracts/script/BlitzNFT.s.sol
@@ -6,11 +6,12 @@ import {BlitzNFT} from "../src/BlitzNFT.sol";
 
 contract BlitzNFTScript is Script {
     function run() public returns (BlitzNFT nft) {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         string memory baseTokenURI = vm.envOr("BASE_TOKEN_URI", string("https://monad-blitz.example.com/api/metadata/"));
         uint256 mintPrice = vm.envOr("MINT_PRICE", uint256(0.1 ether));
         uint256 maxSupply = vm.envOr("MAX_SUPPLY", uint256(1_000));
 
-        vm.startBroadcast();
+        vm.startBroadcast(deployerPrivateKey);
         nft = new BlitzNFT("Monad Blitz", "BLITZ", baseTokenURI, mintPrice, maxSupply);
         vm.stopBroadcast();
     }
